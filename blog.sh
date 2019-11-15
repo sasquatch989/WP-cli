6f542200d2f3287b2aa9b4059b81c8fd7e0d3305
sasquatch989 <solomon.j.sands@gmail.com>
2019-11-15 22:15:07 +0000

    Argparse, usage and basename return

--- blog.sh
+++ blog.sh
@@ -1,11 +1,42 @@
 #!/usr/bin/bash
 
-APP='basename "$0"'
+APP=$(basename "$0")
 
+usage() {
+  printf '\nUsage: blog.sh ACTION[post | category] OPTION[--help|--category] \
+	\n\n\tACTION:\n\t\tpost: Post new blog entry\n\t\tcategory: Create \
+	new category tag\n\nOPTION:\n\t\t--help Display this help content \
+	\n\t\t--category: Assign a category tag to a post'
+  if $1; then
+	  printf '\n%s' "$1"
+  fi
+  
+  exit 1
+}
+
+parse_args() {
+#Takes long-form arguments
+  opts=":-:"
+  if [ $OPTIND -eq 1 ]; then
+	  return_name
+  fi
+  while getopts "$opts" option; do
+     	case "$option" in
+		-)
+		case "$OPTARG" in
+                 	help)  usage;;
+			category) printf 'category';;
+                /?) echo ":Unknown option: -${OPTARG}" >&2; usage;;
+        		
+		esac 
+	esac
+  done
+
+}
 
 return_name() {
 #Returns the name of the running application
-  print "$APP"
+  printf "\n$APP\n"
   exit 0
 }
 
@@ -13,7 +44,7 @@ return_name() {
 
 main() {
   parse_args "$@"
-
+  
 
 }
 
